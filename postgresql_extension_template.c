5154f849718234777c3352f6af7c458fede404b0
Ivan Kovmir <ivan.kovmir@cybertec.at>
2023-11-07 16:32:54 +0100

    Abort build against unsupported server versions
    Abort compilation immediately and obviously against unsupported server
    versions.

--- postgresql_extension_template.c
+++ postgresql_extension_template.c
@@ -3,6 +3,11 @@
 
 PG_MODULE_MAGIC;
 
+/* Abost compilation against unsupported versions. */
+#if PG_VERSION_NUM < 120000
+#error "Unsupported PostgreSQL Version"
+#endif
+
 Datum my_function(PG_FUNCTION_ARGS);
 PG_FUNCTION_INFO_V1(my_function);
 
